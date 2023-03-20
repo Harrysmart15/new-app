58c011fcc66d9e1f19990c4d7adaf464de88b30b
Harrysmart15 <harikarthik508@gmail.com>
2023-03-20 19:51:39 +0530

    commit_first_commit

---
+++ src/data/data.js
@@ -0,0 +1,88 @@
+export const data =[
+    {
+        id   : "108",
+        name : "Tokyo",
+       batch : "Bank of Spain",
+       email : "tokyo@gmail.com",
+       exeprience : 5,
+    },
+
+    {
+        id   : "109",
+        name  : "Professor",
+       batch : "Bank of Spain",
+       email : "profo@gmail.com",
+       exeprience : 20,
+    },
+
+    {
+        id   : "110",
+        name : "Berlin",
+       batch : "Bank of Spain",
+       email : "berlin@gmail.com",
+       exeprience :25,
+    },
+
+
+    {
+        id   : "111",
+        name : "Denver",
+       batch : "Bank of Spain",
+       email : "denver@gmail.com",
+       exeprience : 12,
+    },
+    
+    {
+        id   : "112 ",
+        name : "RIO",
+       batch : "Bank of Spain",
+       email : "denver@gmail.com",
+       exeprience : 5,
+    },
+    
+    {
+        id   : "113",
+        name : "Palermo",
+       batch : "Bank of Spain",
+       email : "denver@gmail.com",
+       exeprience : 12,
+    },
+    
+    {
+        id   : "114",
+        name : "Manila",
+       batch : "Bank of Spain",
+       email : "denver@gmail.com",
+       exeprience : 2,
+    },
+    
+    {
+        id   : "115",
+        name : "Nairobi",
+       batch : "Bank of Spain",
+       email : "denver@gmail.com",
+       exeprience : 7,
+    },
+    {
+        id   : "116",
+        name : "Helenskiy",
+       batch : "Bank of Spain",
+       email : "hel@gmail.com",
+       exeprience : 10,
+    },
+    {
+        id   : "117",
+        name : "Rakel moria",
+       batch : "Bank of Spain",
+       email : "moria@gmail.com",
+       exeprience : 1,
+    }
+
+
+
+
+
+
+
+
+]
